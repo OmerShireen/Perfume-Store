f3729045065a600033895bcde08f64b16b4df34c
Omer Shireen <omer7sce@gmail.com>
2024-12-13 11:44:03 +0500

    Home route Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./screens/Home";
 function App() {
 
 
   return (
      <>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/> 
+        </Routes>
+      </Router>
      </>
   )
 }
