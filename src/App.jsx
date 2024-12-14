a88a02b0b2f9210b57ea8079f442a1dfe9847fca
Omer Shireen <omer7sce@gmail.com>
2024-12-14 09:39:27 +0500

    Login UI and route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./screens/Home";
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import Login from "./screens/Login.jsx";
 function App() {
 
 
   return (
-     <>
+     
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/> 
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
-     </>
+    
   )
 }
   export default App;
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import styles from './Login.module.css'; // Import the CSS module
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <div><Navbar />
+      </div>
+      <div className={styles.loginContainer}>
+        <form className={styles.loginForm}>
+          <h2>Login</h2>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Enter your email" />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter your password" />
+          </div>
+
+          <div className={styles.forgotPassword}>
+            <a href="#">Forgot password?</a>
+          </div>
+
+          <button type="submit" className={styles.btnSignin}>Sign In</button>
+
+          <div className={styles.createAccount}>
+            <p><a href="#">Create Account</a></p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
