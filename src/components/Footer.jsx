0ce58c2ebf8973f44b7aaf8859cde1abaa4e479a
Omer Shireen <omer7sce@gmail.com>
2024-12-14 09:40:47 +0500

    Fotter added and imported

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer
+        className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top"
+        style={{ backgroundColor: '#000000'}}
+      >
+        <div className="col-md-4 d-flex justify-content-center">
+          <span className="" style={{color:'#ffffff'}}>© 2024 Musk Al Mahal</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
+    <>
     <div>
         <Navbar/>
     </div>
+    <div>
+      <Footer/>
+    </div>
+    </>
   )
 }
