4a1f79a93d4d732dbeed6d322553dcf36a7cf03e
Omer Shireen <omer7sce@gmail.com>
2024-12-28 19:15:24 +0500

    Users ui added

---
+++ src/components/UserPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer'
+import ProductList from './Productlist'
+import UserNavbar from './UserNavbar'
+export default function UserPage() {
+  return (
+    <>
+    <UserNavbar/>
+      <ProductList/>
+    <Footer/>
+    </>
+  )
+}
